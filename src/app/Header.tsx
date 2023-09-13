464cf0db961a94b5145749d97f9073d02c1c71ee
Nariman <Nariman@MacBook-Pro--Nariman.local>
2023-09-13 23:37:52 +0400

    day two build

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -10,7 +10,7 @@ function Header() {
     <header>
       <div className='grid grid-cols-3 items-center p-10'>
         <Bars3Icon className='h-8 w-8 text-transparent' />
-        <Link href='/' className='xl:w-[220px] justify-center xl:ml-[125px] ml-0'>
+        <Link href='/' className='xl:w-[220px] justify-center min-[1537px]:ml-[620px] xl:ml-[125px] ml-0'>
             <h1 className='font-serif text-xl md:text-3xl text-center'><span className='underline decoration-6 decoration-red-500'>Nariman&apos;s</span>{" "}news</h1>
         </Link>
 
